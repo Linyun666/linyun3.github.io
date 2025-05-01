511e23ebf2c65c12bf95d70e96b6c9304bc16ffd
Linyun666 <3035601172@qq.com>
2025-05-01 13:17:16 +0800

    Add files via upload

---
+++ gangwei_shouye.js
@@ -0,0 +1,15 @@
+function onClick() {
+  location.href = 'gangweixiangqingye.html';
+}
+
+function onClick_1() {
+  location.href = 'jianli.html';
+}
+
+function onClick_2() {
+  location.href = 'mianshi.html';
+}
+
+function onClick_3() {
+  location.href = 'geren.html';
+}
---
+++ jianli.js
@@ -0,0 +1,11 @@
+function onClick() {
+  location.href = 'index.html';
+}
+
+function onClick_1() {
+  location.href = 'mianshi.html';
+}
+
+function onClick_2() {
+  location.href = 'geren.html';
+}
